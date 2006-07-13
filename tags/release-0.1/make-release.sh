bdda67a1caba267ed4a680d57eed295105ae93a7
wiseman <wiseman@47c02b34-914f-0410-97cb-95abd8a3feca>
2006-07-13 23:14:22 +0000

    Release 0.1

---
+++ tags/release-0.1/make-release.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -rf docs lucene-in-action clean*
+rm -rf `find . -name .svn`
+rm make-release.sh
+cd ..
+mv montezuma montezuma-0.1
+tar czvf montezuma-0.1.tar.gz montezuma-0.1
---
+++ tags/release-0.1/src/util/mop.lisp
@@ -0,0 +1,62 @@
+(in-package #:montezuma)
+
+;; Used to support the object cloning protocol.  I don't like that
+;; this isn't standard, and I have a hunch that the cloning protocol
+;; should go away.  But it is pretty convenient...
+
+(defun class-slots (class)
+  #+(or digitool openmcl) (ccl:class-slots class)
+  #+sbcl (sb-mop:class-slots class)
+  #+allegro (mop:class-slots class))
+  
+(defun slot-definition-name (slot-defn)
+  #+(or digitool openmcl) (ccl:slot-definition-name slot-defn)
+  #+sbcl (sb-mop:slot-definition-name slot-defn)
+  #+allegro (mop:slot-definition-name slot-defn))
+
+
+;; Something approximating the Ruby clone protocol.
+
+(defun clone (object)
+  (let ((clone (clone-object object)))
+    (initialize-copy clone object)
+    clone))
+
+(defgeneric clone-object (object))
+(defgeneric initialize-copy (object clonee))
+
+(defmethod clone-object ((object T))
+  (let ((copy (allocate-instance (class-of object))))
+    (loop for slot in (class-slots (class-of object))
+       when (slot-boundp object (slot-definition-name slot))
+       do (setf (slot-value copy (slot-definition-name slot))
+		(slot-value object (slot-definition-name slot))))
+    copy))
+
+(defmethod initialize-copy (self o)
+  (declare (ignore self) (ignore o)))
+
+(defmethod clone-object ((array array))
+  (multiple-value-bind (displaced-to displaced-index-offset)
+      (array-displacement array)
+    (let ((dimensions (array-dimensions array))
+          (element-type (array-element-type array))
+          (adjustable (adjustable-array-p array))
+          (fill-pointer (when (array-has-fill-pointer-p array)
+                          (fill-pointer array))))
+      (let ((new-array
+             (apply #'make-array
+                    (list* dimensions
+                           :element-type element-type
+                           :adjustable adjustable
+                           :fill-pointer fill-pointer
+                           :displaced-to displaced-to
+                           (if displaced-to
+                               (list :displaced-index-offset
+                                     displaced-index-offset)
+                               nil)))))
+        (unless displaced-to
+          (dotimes (i (array-total-size array))
+            (setf (row-major-aref new-array i)
+                  (row-major-aref array i))))
+        new-array))))
